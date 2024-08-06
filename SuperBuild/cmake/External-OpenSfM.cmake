8b193e4b75afea279fa4bc8432f3b038bfab7d1d
LouiseLog <gaby@louiselog.net>
2024-08-05 23:24:28 -0300

    settings.yalm, opensfm

--- SuperBuild/cmake/External-OpenSfM.cmake
+++ SuperBuild/cmake/External-OpenSfM.cmake
@@ -24,7 +24,7 @@ ExternalProject_Add(${_proj_name}
   STAMP_DIR         ${_SB_BINARY_DIR}/stamp
   #--Download step--------------
   DOWNLOAD_DIR      ${SB_DOWNLOAD_DIR}
-  GIT_REPOSITORY    https://github.com/OpenDroneMap/OpenSfM/
+  GIT_REPOSITORY    https://github.com/LouiseLog/OpenSfM/
   GIT_TAG           352
   #--Update/Patch step----------
   UPDATE_COMMAND    git submodule update --init --recursive
